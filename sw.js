bc7f51f6328b70ea65bf95a79ed164ac1e771aa6
EmergeEnvironmentalServicesPtyLtd <joel.chamberlain@emergeassociates.com.au>
2025-10-06 13:47:29 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker Installed");
-});
-self.addEventListener("fetch", (e) => {});
+const CACHE = "emerge-checklist-v1";
+self.addEventListener("install", (e) => { self.skipWaiting(); });
+self.addEventListener("activate", (e) => { e.waitUntil(self.clients.claim()); });
+self.addEventListener("fetch", (e) => { /* passthrough */ });
